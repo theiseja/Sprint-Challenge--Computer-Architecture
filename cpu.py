eff862efb5d906cca3f712a36b61905f5ab52f55
Jesse Theisen <theiseja@gmail.com>
2020-05-02 13:09:44 -0500

    Completes cpu.py, MVP complete

--- cpu.py
+++ cpu.py
@@ -82,3 +82,164 @@ class CPU:
 
         print("JUMPING")
         self.PC = address
+
+    def JEQ(self):
+        """Jumps to address stored in given register if equal flag is set to true"""
+
+        address = self.reg[self.operand_a]
+
+        if self.FL & 0b0000000 == 1:
+            self.PC = address
+
+        else:
+            self.PC += 2
+
+    def JNE(self):
+        """jumps to the address stored in given register if e flag is clear (false, 0)"""
+        address = self.reg[self.operand_a]
+
+        if self.FL & 0b00000001 == 0:
+            self.PC = address
+        else:
+            self.PC += 2
+
+    def HALT(self):
+        """exits the program"""
+        sys.exit()
+
+    def LOAD(self):
+        """Loads value to register"""
+        self.reg[self.operand_a] = self.oeprand_b
+
+    def PRINT(self):
+        """Prints the value that is currently stored in a register"""
+        print(self.reg[self.operand_a])
+
+    def PUSH(self):
+        """Pushes the value in the given register to the top of the stack"""
+        # decrement the SP
+        global SP
+
+        self.reg[SP] -= 1
+
+        # copies the value in the given register to the SP address
+        value = self.reg[self.operand_a]
+
+        self.ram[self.reg[SP]] = value
+
+    def POP(self):
+        """Pops the value at the top of the stack into the register"""
+        global SP
+        # copies value of address pointed to by SP to the register"""
+        value = self.ram[self.reg[SP]]
+
+        # given register from argument
+        register = self.operand_a
+
+        self.reg[register] = value
+
+        # increment SP
+        self.reg[SP] += 1
+
+    def ram_read(self, address):
+        """Accepts an address to read and return the value that it has stored"""
+        self.MAR = address
+        self.MDR = self.ram[address]
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        """Accepts a value to write and what address to write the value to"""
+        self.MAR = address
+        self.MDR = value
+        self.ram[address] = value
+
+    def load(self):
+        """Loads a program into memory"""
+        if len(sys.argv) != 2:
+            print("ERROR: Must input file name")
+            sys.exit(1)
+        filename = sys.argv[1]
+
+        try:
+            address = 0
+            # opens the file
+            with open(filename) as program:
+                # reads all the lines in the file
+                for instruction in program:
+                    # parses out comments
+                    comment_split = instruction.strip().split("#")
+                    # casts the number from strings to ints
+                    value = comment_split[0].strip()
+
+                    # ignore blank lines
+                    if value == "":
+                        continue
+                    num = int(value, 2)
+                    self.ram[address] = num
+                    address += 1
+
+        except FileNotFoundError:
+            print("File not found")
+            sys.exit(2)
+
+    def ALU(self, op, reg_a, reg_b):
+        """ALU operations"""
+        if op == math_op["ADD"]:
+            print("ADDING: ")
+            self.reg[reg_a] += self.reg[reg_b]
+
+        elif op == math_op["SUB"]:
+            print("SUBTRACTING: ")
+            self.reg[reg_a] -= self.reg[reg_b]
+
+        elif op == math_op["MUL"]:
+            print("MULTIPLYING: ")
+            self.reg[reg_a] *= self.reg[reg_b]
+
+        elif op == math_op["CMP"]:
+            valueA = self.reg[self.operand_a]
+            valueB = self.reg[self.operand_b]
+
+            if valueA == valueB:
+                self.FL = 0b00000001
+
+            if valueA < valueB:
+                self.FL = 0b00000100
+
+            if valueA > valueB:
+                self.FL = 0b00000010
+        else:
+            raise Exception("Unsupported ALU operation")
+
+    def trace(self):
+
+        print(f"TRACE: %02X | %02X %02X %02X |" % (
+            self.PC,
+            self.ram_read(self.PC),
+            self.ram_read(self.PC + 1),
+            self.ram_read(self.PC + 2)
+        ), end='')
+
+        for i in range(8):
+            print(" %02X" % self.reg[i], end='')
+
+    def move_PC(self, IR):
+        if (IR << 3) % 255 >> 7 != 1:
+            self.PC += (IR >> 6) + 1
+
+    def run(self):
+        """Run the CPU."""
+        while True:
+            IR = self.ram_read(self.PC)
+            self.operand_a = self.ram_read(self.PC + 1)
+            self.operand_b = self.ram_read(self.PC + 2)
+            if (IR << 2) % 255 >> 7 == 1:
+                self.ALU(IR, self.operand_a, self.operand_b)
+                self.move_PC(IR)
+            elif (IR << 2) % 255 >> 7 == 0:
+                self.instructions[binary_op[IR]]()
+                self.move_PC(IR)
+            else:
+                print(f"Space dog did not understand that command: {IR}")
+                print(self.trace())
+                sys.exit(1)
