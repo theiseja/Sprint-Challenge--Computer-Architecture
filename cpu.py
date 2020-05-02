0a5234e820dbb441b8773e338e60b2cd0063746f
Jesse Theisen <theiseja@gmail.com>
2020-05-02 13:53:29 -0500

    MVP complete

--- cpu.py
+++ cpu.py
@@ -9,25 +9,25 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8 
-        self.sp = 7
-        self.reg[self.sp] = 0xF4 # Stack pointer starts at F4 or 244 in RAM
-        self.pc = 0 # Program Counter, address of the currently executing instruction
-        self.ir = 0 # Instruction Register, contains a copy of the currently executing instruction
-        self.fl = 6 # Flag Register 
+        self.SP = 7
+        self.reg[self.SP] = 0xF4 # Stack pointer starts at F4 or 244 in RAM
+        self.PC = 0 # Program Counter, address of the currently executing instruction
+        self.IR = 0 # Instruction Register, contains a copy of the currently executing instruction
+        self.FL = 6 # Flag Register 
         self.op_table = {
-                        0b10000010 : self.ldi, 
-                        0b01000111 : self.prn, 
-                        0b10100010 : self.mult,
-                        0b00000001 : self.hlt,
-                        0b01000101 : self.push,
-                        0b01000110 : self.pop,
-                        0b01010000 : self.call, 
-                        0b00010001 : self.ret,
-                        0b10100000 : self.add, 
-                        0b01010110 : self.jne,
-                        0b01010100 : self.jmp,
-                        0b01010101 : self.jeq, 
-                        0b10100111 : self.cmpp}
+                        0b10000010 : self.LDI, 
+                        0b01000111 : self.PRN, 
+                        0b10100010 : self.MUL,
+                        0b00000001 : self.HLT,
+                        0b01000101 : self.PUSH,
+                        0b01000110 : self.POP,
+                        0b01010000 : self.CALL, 
+                        0b00010001 : self.RET,
+                        0b10100000 : self.ADD, 
+                        0b01010110 : self.JNE,
+                        0b01010100 : self.JMP,
+                        0b01010101 : self.JEQ, 
+                        0b10100111 : self.CMP}
 
     def load(self):
         """Load a program into memory."""
@@ -49,76 +49,76 @@ class CPU:
                 continue
             address+=1
     
-    def ldi(self, operand_a, operand_b):
+    def LDI(self, operand_a, operand_b):
         self.reg[operand_a] = operand_b
 
-    def mult(self, operand_a, operand_b):
+    def MUL(self, operand_a, operand_b):
         self.reg[operand_a] = self.reg[operand_a] * self.reg[operand_b]
     
     # Push the value in the given register on the stack.
-    def push(self, operand_a, operand_b):        
+    def PUSH(self, operand_a, operand_b):        
         self.reg[self.sp] -= 1
         self.ram[self.reg[self.sp]] = self.reg[operand_a]
     
     # Pop the value at the top of the stack into the given register.
-    def pop(self, operand_a, operand_b):
+    def POP(self, operand_a, operand_b):
         self.reg[operand_a] = self.ram[self.reg[self.sp]]
         self.reg[self.sp] += 1
 
     # Sets the PC to the register value
-    def call(self, operand_a, operand_b):
+    def CALL(self, operand_a, operand_b):
         # address of instruction after call is pushed on to the stack
         self.reg[self.sp] -= 1
-        self.ram[self.reg[self.sp]] = self.pc + 2
+        self.ram[self.reg[self.sp]] = self.PC + 2
         # set PC to value stored in given register
-        self.pc = self.reg[operand_a]
+        self.PC = self.reg[operand_a]
         return True
     
-    def ret(self, operand_a, operand_b):
+    def RET(self, operand_a, operand_b):
         self.pop(operand_a, 0)
-        self.pc = self.reg[operand_a]
+        self.PC = self.reg[operand_a]
         return True
 
-    def add(self, operand_a, operand_b):
+    def ADD(self, operand_a, operand_b):
         self.reg[operand_a] = self.reg[operand_a] + self.reg[operand_b]
     
-    def cmpp(self, operand_a, operand_b):
+    def CMP(self, operand_a, operand_b):
         value_a = self.reg[operand_a]
         value_b = self.reg[operand_b]
 
         if value_a == value_b:
-            self.reg[self.fl] = 1
+            self.reg[self.FL] = 1
         elif value_a > value_b:
-            self.reg[self.fl] = 2
+            self.reg[self.FL] = 2
         else:
-            self.reg[self.fl] = 4
+            self.reg[self.FL] = 4
 
 
-    def jne(self, operand_a, operand_b):
-        value = self.reg[self.fl]
+    def JNE(self, operand_a, operand_b):
+        value = self.reg[self.FL]
         if value == 2 or value == 4: #not equal
-            return self.jmp(operand_a, 0)
+            return self.JMP(operand_a, 0)
 
-    def jmp(self, operand_a, operand_b):
-        self.pc = self.reg[operand_a]
+    def JMP(self, operand_a, operand_b):
+        self.PC = self.reg[operand_a]
         return True
 
-    def jeq(self, operand_a, operand_b):
-        if self.reg[self.fl] == 1:
-            return self.jmp(operand_a, 0)
+    def JEQ(self, operand_a, operand_b):
+        if self.reg[self.FL] == 1:
+            return self.JMP(operand_a, 0)
     def ram_read(self, address):
         return self.ram[address]
     
     def ram_write(self, value, address):
         self.ram[address] = value
     
-    def prn(self, operand_a, operand_b):
+    def PRN(self, operand_a, operand_b):
         print(self.reg[operand_a])
 
-    def hlt(self, operand_a, operand_b):
+    def HLT(self, operand_a, operand_b):
         sys.exit()
 
-    def alu(self, op, reg_a, reg_b):
+    def ALU(self, op, reg_a, reg_b):
         """ALU operations."""
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
@@ -128,10 +128,10 @@ class CPU:
 
     def trace(self):
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.PC,
+            self.ram_read(self.PC),
+            self.ram_read(self.PC + 1),
+            self.ram_read(self.PC + 2)
         ), end='')
 
         for i in range(8):
@@ -142,11 +142,11 @@ class CPU:
     def run(self):
         """Run the CPU."""
         while True:
-            self.ir = self.ram[self.pc]
-            operand_a = self.ram[self.pc + 1]
-            operand_b = self.ram[self.pc + 2]
+            self.IR = self.ram[self.PC]
+            operand_a = self.ram[self.PC + 1]
+            operand_b = self.ram[self.PC + 2]
 
-            willJump = self.op_table[self.ir](operand_a, operand_b)
+            willJump = self.op_table[self.IR](operand_a, operand_b)
 
             if not willJump:
-                self.pc += (self.ir >> 6) + 1;
+                self.PC += (self.IR >> 6) + 1;
